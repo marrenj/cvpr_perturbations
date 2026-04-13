ee7a7f3b390861152811dad2a63f79ddb5ce1c64
marrenj <mej.jenkins@gmail.com>
2026-04-13 10:45:27 -0500

    Small changes

--- run_epoch_length_sweep_cuda1.sh
+++ run_epoch_length_sweep_cuda1.sh
@@ -31,7 +31,7 @@ START_EPOCHS=(0 2 4 6 8 14 19 29 39 49 59 69 79 89 99)
 PERTURB_LENGTHS=(5 10 20 30 40 50)
 PERTURB_SEEDS=(1 2 3)
 # Resume control: empty or "None" means start from the beginning; otherwise skip until the named run.
-RESUME_FROM="vit_l_14_rank32_perturb-type-image_noise_epoch4_length10_perturb-seed2_init-seed2behavioral-rsa-True"
+RESUME_FROM="vit_l_14_rank32_perturb-type-image_noise_epoch4_length5_perturb-seed3_init-seed3behavioral-rsa-True"
 if [[ "$RESUME_FROM" == "None" || -z "$RESUME_FROM" ]]; then
   FOUND=true
 else
